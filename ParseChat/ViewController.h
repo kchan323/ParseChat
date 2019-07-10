dc8ac96669899140de8295b1255677117758d34a
Katelin Chan <kchan23@fb.com>
2019-07-10 10:36:54 -0700

    Initial Commit

---
+++ ParseChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by kchan23 on 7/10/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParseChat/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ParseChat
+//
+//  Created by kchan23 on 7/10/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
